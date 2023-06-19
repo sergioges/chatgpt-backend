aff544688860729a3651317142f8e492c607587a
unknown <sergio@primerocomunico.es>
2023-06-19 18:32:29 +0200

    add is_local variable for controlling access to MongoDB

--- config/db.py
+++ config/db.py
@@ -5,10 +5,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 mongodb_access = getenv("MONGODB_ACCESS")
+is_local = getenv("IS_LOCAL")
 
-# connection = MongoClient("mongodb://localhost")
-
-connection = MongoClient(f"mongodb+srv://sergioges:{mongodb_access}@chatgpt-app.xqppbuf.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
+if is_local == "True":
+    connection = MongoClient("mongodb://localhost")
+else:
+    connection = MongoClient(f"mongodb+srv://sergioges:{mongodb_access}@chatgpt-app.xqppbuf.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
 
 # client = MongoClient("mongodb://localhost:27017/")
 # db = client["chatgptDB"]
