e19165189734343b56b8da26f46f2166b23ab92d
unknown <sergio@primerocomunico.es>
2023-06-20 11:43:00 +0200

    test cors middleware

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
